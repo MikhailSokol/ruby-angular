0ca7df2dec6d18d463918ec765a11010776e2d8a
mike <https://github.com/MikhailSokol>
2022-04-14 00:52:15 +0300

    Procfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   
-  
+  get "/" , to: redirect('/projects')
   post "/todos", to: "todos#create"
   get "/projects", to: "projects#index"
   patch "/projects/:id/todo/:id", to: "projects#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'static/index'
-  #get '*other', to: 'static#index'
+  get '/projects', to: 'static#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
