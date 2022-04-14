d5d0852417afd75c670e55168cf99c51a56cefa6
mike <https://github.com/MikhailSokol>
2022-04-14 11:56:54 +0300

    форматирование

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import { Observable, tap } from 'rxjs';
-import {MatDialog} from '@angular/material/dialog';
-import {ModalFormComponent} from './modal-form/modal-form.component';
-import { ModalformService } from './services/modalform.service';
-
+import { Component, Input, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, tap } from "rxjs";
+import { MatDialog } from "@angular/material/dialog";
+import { ModalFormComponent } from "./modal-form/modal-form.component";
+import { ModalformService } from "./services/modalform.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  
- 
- constructor(private http:HttpClient,public dialog: MatDialog,public TodoServices:ModalformService){
-   }
+  constructor(
+    private http: HttpClient,
+    public dialog: MatDialog,
+    public TodoServices: ModalformService
+  ) {}
 
   openDialog() {
-      this.dialog.open(ModalFormComponent);
-      
+    this.dialog.open(ModalFormComponent);
   }
-  ngOnInit(){
-    this.TodoServices.fetch().subscribe((val)=>{
-      this.TodoServices.checkingLengthOfArrayTodo()
-    })
-    
+  ngOnInit() {
+    this.TodoServices.fetch().subscribe((val) => {
+      this.TodoServices.checkingLengthOfArrayTodo();
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {ModalFormComponent} from './modal-form/modal-form.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { plainToClass } from 'class-transformer';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { ModalFormComponent } from "./modal-form/modal-form.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { ScrollingModule } from "@angular/cdk/scrolling";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { plainToClass } from "class-transformer";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-   ModalFormComponent,
-  
-  ],
+  declarations: [AppComponent, ModalFormComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -41,12 +37,11 @@ import { AppRoutingModule } from './app-routing.module';
     MatButtonModule,
     MatInputModule,
     MatSelectModule,
-    FlexLayoutModule ,
+    FlexLayoutModule,
     ScrollingModule,
     MatCheckboxModule,
-
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
